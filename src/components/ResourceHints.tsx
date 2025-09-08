646706541e37257c828cd7428ac0ccbe6acf610c
neolite <ufadiz@gmail.com>
2025-09-08 15:27:42 +0300

    update first images

--- src/components/ResourceHints.tsx
+++ src/components/ResourceHints.tsx
@@ -6,23 +6,34 @@ export const ResourceHints = () => {
         rel="preload"
         as="image"
         fetchPriority="high"
-        href="/images/hero-poster.jpg"
+        href="/images/hero-poster.webp"
       />
       <link
-        rel="preload" 
+        rel="preload"
         as="image"
         fetchPriority="high"
-        href="/images/hero-poster-mobile-cropped.jpg"
+        href="/images/hero-poster-mobile-cropped.webp"
         media="(max-width: 899px)"
       />
-      
+
       {/* Preconnect to Strapi API for faster API calls */}
-      <link rel="preconnect" href="https://lovely-idea-e9a72cf425.strapiapp.com" />
-      <link rel="preconnect" href="https://lovely-idea-e9a72cf425.media.strapiapp.com" />
-      <link rel="dns-prefetch" href="https://lovely-idea-e9a72cf425.strapiapp.com" />
-      <link rel="dns-prefetch" href="https://lovely-idea-e9a72cf425.media.strapiapp.com" />
-      
-      
+      <link
+        rel="preconnect"
+        href="https://lovely-idea-e9a72cf425.strapiapp.com"
+      />
+      <link
+        rel="preconnect"
+        href="https://lovely-idea-e9a72cf425.media.strapiapp.com"
+      />
+      <link
+        rel="dns-prefetch"
+        href="https://lovely-idea-e9a72cf425.strapiapp.com"
+      />
+      <link
+        rel="dns-prefetch"
+        href="https://lovely-idea-e9a72cf425.media.strapiapp.com"
+      />
+
       {/* Prefetch most likely client logo images for performance */}
       <link rel="prefetch" as="image" href="/client-logos/siemens.webp" />
       <link rel="prefetch" as="image" href="/client-logos/porsche.webp" />
